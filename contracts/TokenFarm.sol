ca84a29f9e0c7c257826720823a2751157bb89eb
Tomi Lui <tomilui1221@gmail.com>
2021-06-23 14:28:03 -0700

    update

--- contracts/TokenFarm.sol
+++ contracts/TokenFarm.sol
@@ -1,6 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.7.4;
 
+import "./FijiToken.sol";
+import "./DaiToken.sol";
+
+
 contract TokenFarm {
     string public name = "Fiji Token Farm";
+
+    DaiToken public daiToken;
+    FijiToken public fijiToken;
+
+    constructor(FijiToken _fijiToken, DaiToken _daiToken ) {
+        daiToken = _daiToken;
+        fijiToken = _fijiToken;
+    }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,24 @@ const DaiToken = artifacts.require("DaiToken");
 const FijiToken = artifacts.require("FijiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  // deployer.deploy(FijiToken);
-  // deployer.deploy(DaiToken);
+module.exports = async function(deployer, network, accounts) {
+  
+
+  // deploy Fiji Token
+  await deployer.deploy(FijiToken);
+  const fijiToken = await FijiToken.deployed()
+
+  // deploy Dai Token
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed()
+
+  // deploy Token Farm
   deployer.deploy(TokenFarm);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
+  const tokenFarm = await TokenFarm.deployed()
+
+  // Transfer all tokens to TokenFarm (1 mill)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  // Transfer 100 DAI tokens to investor
+  await daiToken.transfer(accounts[1],'100000000000000000000')
 };
